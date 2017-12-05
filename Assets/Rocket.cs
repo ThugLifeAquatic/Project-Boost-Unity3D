fae5e60db38758725b25d6d2c41c0b9bb6a6c0a9
Austin Cohrs <acohrsdev@outlook.com>
2017-12-05 15:19:24 -0800

    Tuned thrust and rotation, Added drag, added additional planet and landing pad.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,25 +5,68 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField]
+    float rcsThrust = 100f;
+    [SerializeField]
+    float mainThrust = 1000f;
+
+
     Rigidbody rigidBody;
     AudioSource audioSource;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
+        AudioSwitcher(Thrust(), Rotate());
 	}
 
-    private void ProcessInput()
+    private bool Rotate()
     {
+        rigidBody.freezeRotation = true;
+
+        float rotationMultiplier = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.back * rotationMultiplier);
+            return true;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.forward * rotationMultiplier);
+            return true;
+        }
+
+        rigidBody.freezeRotation = false;
+        return false;
+    }
+
+    private bool Thrust()
+    {
+        float thrustMultiplier = mainThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * thrustMultiplier);
+            return true;
+        }
+        return false;
+    }
+
+    private void AudioSwitcher(bool thrusting, bool rotating)
+    {
+        if (thrusting == true && rotating == true)
+        {
+            audioSource.pitch = 2;
+        }
+        else if (thrusting == true && rotating == false)
+        {
+            audioSource.pitch = 1;
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -33,17 +76,5 @@ public class Rocket : MonoBehaviour {
         {
             audioSource.Stop();
         }
-        if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Rotate(Vector3.back * Time.deltaTime * 5);
-            audioSource.pitch = 1;
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Rotate(Vector3.forward * Time.deltaTime * 5);
-            audioSource.pitch = 1;
-        }
-
     }
 }
